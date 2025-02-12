5f00b897ee583be22f56ad68fc504f3ff71bb981
unknown <qhliang1002@gmail.com>
2025-02-12 19:40:18 +0000

    sphere rendering

--- RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing.cpp
@@ -3,10 +3,40 @@
 #include "../color.h"
 #include "../ray.h"
 
+
+point3 g_sphereCentre = point3(0.0, 0.0, -1.0);
+
+double hitSphere(const ray& r, const point3& sphereCentre, double radius)
+{
+    vec3 CQ = sphereCentre - r.origin();
+    double a = r.direction().length_sqr();
+    double b = -2.0 * dot(r.direction(), CQ);
+    double c = CQ.length_sqr() - radius * radius;
+
+    double determ = b * b - 4 * a * c;
+
+    if (determ < 0.0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - std::sqrt(determ)) / (2 * a);
+    }
+}
+
 color ray_color(const ray& r)
 {
+    double t = hitSphere(r, g_sphereCentre, 0.5);
+    if (t >= 0.0)
+    {
+        vec3 normal = unit_vector(r.at(t) - g_sphereCentre);
+        return 0.5 * color(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
+
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
